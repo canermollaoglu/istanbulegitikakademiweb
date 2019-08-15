79817973f638be92bbb50bfd71ca53ddc579a854
Mesut Öztürk <mesutoztrk@msn.com>
2019-08-15 23:02:02 +0300

    kategori crud1

--- NitelikliBilisim.App/Areas/Admin/Controllers/DataController.cs
+++ NitelikliBilisim.App/Areas/Admin/Controllers/DataController.cs
@@ -1,6 +1,11 @@
 using System;
+using DevExtreme.AspNet.Data;
 using Microsoft.AspNetCore.Mvc;
-using NitelikliBilisim.Core.ViewModels;
+using NitelikliBilisim.App.Extensions;
+using NitelikliBilisim.Core.Entities;
+using NitelikliBilisim.Core.Repositories;
+using System.Linq;
+using DevExtreme.AspNet.Data.ResponseModel;
 
 namespace NitelikliBilisim.App.Areas.Admin.Controllers
 {
@@ -8,6 +13,27 @@ namespace NitelikliBilisim.App.Areas.Admin.Controllers
     [ApiController]
     public class DataController : Controller
     {
-        
+        private readonly IRepository<Kategori, Guid> _kategoryRepo;
+
+        public DataController(IRepository<Kategori, Guid> kategoryRepo)
+        {
+            _kategoryRepo = kategoryRepo;
+            if (_kategoryRepo.GetAll().Count()<100)
+            {
+                _kategoryRepo.Add(new Kategori()
+                {
+                    Ad = "Test Kategori"+new Random().Next(),
+                    Aciklama = "Test açıklama"+new Random().Next()
+                });
+            }
+        }
+
+        public LoadResult GetKategoriData(DataSourceLoadOptions loadOptions)
+        {
+            loadOptions.PrimaryKey = new[] {"Id"};
+
+            var data = _kategoryRepo.GetAll().OrderBy(x=>x.CreatedDate);
+            return DataSourceLoader.Load(data, loadOptions);
+        }
     }
 }
