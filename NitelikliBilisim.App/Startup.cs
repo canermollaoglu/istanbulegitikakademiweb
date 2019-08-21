b39b22fa53e14fea598b9ccaa94334834a5e8cee
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-08-21 11:40:02 +0300

    Update Startup.cs

--- NitelikliBilisim.App/Startup.cs
+++ NitelikliBilisim.App/Startup.cs
@@ -48,6 +48,8 @@ namespace NitelikliBilisim.App
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
