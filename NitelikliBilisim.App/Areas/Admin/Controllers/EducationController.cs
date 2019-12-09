989731f7c9ff87cfb325732ead89d49b336f37b2
aksungursanberk <sanberkaksungur@outlook.com>
2019-12-09 12:43:36 +0300

    Kurs Tanımlama #19

---
+++ NitelikliBilisim.App/Areas/Admin/Controllers/EducationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NitelikliBilisim.App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EducationController : Controller
+    {
+        [Route("admin/egitim-ekle")]
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+    }
+}
--- NitelikliBilisim.Core/Enums/educations/EducationLevel.cs
+++ NitelikliBilisim.Core/Enums/educations/EducationLevel.cs
@@ -1,9 +1,14 @@
-﻿namespace NitelikliBilisim.Core.Enums
+﻿using System.ComponentModel;
+
+namespace NitelikliBilisim.Core.Enums
 {
     public enum EducationLevel
     {
+        [Description("Başlangıç")]
         Beginner = 1010,
+        [Description("Orta")]
         Intermediate = 1020,
+        [Description("İleri")]
         Advanced = 1030
     }
 }
