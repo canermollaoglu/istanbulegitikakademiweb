b621ec9b0528abf96ef0aca685f657e21d3100d1
aksungursanberk <sanberkaksungur@outlook.com>
2019-12-12 15:42:35 +0300

    Kurs Tanımlama #19

--- NitelikliBilisim.App/Areas/Admin/Controllers/EducationPartController.cs
+++ NitelikliBilisim.App/Areas/Admin/Controllers/EducationPartController.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NitelikliBilisim.App.Areas.Admin.VmCreator.EducationParts;
 using NitelikliBilisim.App.Models;
+using NitelikliBilisim.App.Utility;
 using NitelikliBilisim.Business.UoW;
+using NitelikliBilisim.Core.ViewModels.areas.admin.education_parts;
 
 namespace NitelikliBilisim.App.Areas.Admin.Controllers
 {
@@ -50,10 +52,47 @@ namespace NitelikliBilisim.App.Areas.Admin.Controllers
         }
 
         [HttpPost, Route("admin/add-education-part")]
-        public IActionResult AddPart()
+        public IActionResult AddPart(AddPartVm data)
         {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelStateUtil.GetErrors(ModelState);
+                return Json(new ResponseModel
+                {
+                    isSuccess = false,
+                    errors = errors
+                });
+            }
 
-            return Json("");
+            _unitOfWork.EducationPart.Insert(new Core.Entities.EducationPart
+            {
+                Title = data.Title,
+                EducationId = data.EducationId,
+                Order = data.Order.GetValueOrDefault(0),
+                Duration = data.Duration.GetValueOrDefault(0)
+            });
+
+            return Json(new ResponseModel
+            {
+                isSuccess = true
+            });
+        }
+
+        [Route("admin/delete-education-part/{partId}")]
+        public IActionResult DeletePart(Guid? partId)
+        {
+            if (partId == null)
+                return Json(new ResponseModel
+                {
+                    isSuccess = false,
+                    errors = new List<string> { "Eğitimin eğitim parçasını silerken bir hata oluştu" }
+                });
+
+            _unitOfWork.EducationPart.Delete(partId.Value);
+            return Json(new ResponseModel
+            {
+                isSuccess = true
+            });
         }
     }
 }
--- NitelikliBilisim.Core/ViewModels/areas/admin/education_parts/ManageVm.cs
+++ NitelikliBilisim.Core/ViewModels/areas/admin/education_parts/ManageVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace NitelikliBilisim.Core.ViewModels.areas.admin.education_parts
@@ -8,6 +9,17 @@ namespace NitelikliBilisim.Core.ViewModels.areas.admin.education_parts
     {
         public Guid EducationId { get; set; }
         public string EducationName { get; set; }
+    }
+
+    public class AddPartVm
+    {
+        public Guid EducationId { get; set; }
+        [Required(ErrorMessage = "Sıra boş geçilemez"), Range(1, 100, ErrorMessage = "Sıra 1 ile 100 arasında bir değer olmalıdır")]
+        public byte? Order { get; set; }
+        [Required(ErrorMessage = "Başlık boş geçilemez")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Süre boş geçilemez"), Range(1, 255, ErrorMessage = "Süre 1 ile 255 arasında bir değer olmalıdır")]
+        public byte? Duration { get; set; }
     }
 
     public class GetEducationPartsVm
