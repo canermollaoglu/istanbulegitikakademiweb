d70e087cce883546734c7956a3ab94eb66c20ffc
aksungursanberk <sanberkaksungur@outlook.com>
2019-12-12 11:52:17 +0300

    Kurs Tanımlama #19

---
+++ NitelikliBilisim.App/Areas/Admin/Controllers/EducationPartController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NitelikliBilisim.App.Models;
+
+namespace NitelikliBilisim.App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EducationPartController : Controller
+    {
+        [Route("admin/egitim-parca-yonetimi/{educationId}")]
+        public IActionResult Manage(Guid? educationId)
+        {
+            if (educationId == null)
+                return Redirect("/");
+
+            return View();
+        }
+
+        [Route("admin/get-education-parts/{educationId}")]
+        public IActionResult GetEducationParts(Guid? educationId)
+        {
+            if (educationId == null)
+                return Json(new ResponseModel
+                {
+                    isSuccess = false,
+                    errors = new List<string> { "Eğitimin parçalarını getirirken bir hata oluştu" }
+                });
+
+            return Json(new ResponseModel
+            {
+                isSuccess = true
+            });
+        }
+    }
+}
