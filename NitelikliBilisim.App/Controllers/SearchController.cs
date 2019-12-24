a9ee781ab000dc4b63973d4c42df714b6df123ff
aksungursanberk <sanberkaksungur@outlook.com>
2019-12-24 16:26:42 +0300

    Anasayfa kurs arama #27

---
+++ NitelikliBilisim.App/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NitelikliBilisim.App.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult MainSearch(string searchText)
+        {
+            return Json("");
+        }
+    }
+}
