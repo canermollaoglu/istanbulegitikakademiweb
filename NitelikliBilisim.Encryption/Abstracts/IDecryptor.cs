ef4ec6c622f3c447cdde1d83b1f3709c8381c39d
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2020-01-06 11:25:46 +0300

    #52

--- NitelikliBilisim.Encryption/Abstracts/IDecryptor.cs
+++ NitelikliBilisim.Encryption/Abstracts/IDecryptor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NitelikliBilisim.Encryption.Abstracts
+﻿namespace NitelikliBilisim.Encryption.Abstracts
 {
     public interface IDecryptor
     {
