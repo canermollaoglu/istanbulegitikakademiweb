bf1cb6325edf12bbaa2e01e8bd9c3984193eaf1e
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2020-01-06 11:00:47 +0300

    :rocket: /test-ready

--- NitelikliBilisim.Core/Entities/user_details/Customer.cs
+++ NitelikliBilisim.Core/Entities/user_details/Customer.cs
@@ -1,10 +1,7 @@
 ﻿using NitelikliBilisim.Core.Abstracts;
 using NitelikliBilisim.Core.Enums;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace NitelikliBilisim.Core.Entities
 {
