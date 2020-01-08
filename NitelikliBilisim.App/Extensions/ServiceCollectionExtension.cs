88b895dce5cc3e66ef49dd60d598f31f2e7a3712
aksungursanberk <sanberkaksungur@outlook.com>
2020-01-08 15:05:04 +0300

    Güçlü şifre ayarları kapatıldı Öğrenci Kayıt Sayfası #47

--- NitelikliBilisim.App/Extensions/ServiceCollectionExtension.cs
+++ NitelikliBilisim.App/Extensions/ServiceCollectionExtension.cs
@@ -43,10 +43,10 @@ namespace NitelikliBilisim.App.Extensions
             services.Configure<IdentityOptions>(options =>
             {
                 // Password settings.
-                options.Password.RequireDigit = true;
+                options.Password.RequireDigit = false;
                 options.Password.RequireLowercase = false;
                 options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = true;
+                options.Password.RequireUppercase = false;
                 options.Password.RequiredLength = 6;
                 options.Password.RequiredUniqueChars = 0;
 
