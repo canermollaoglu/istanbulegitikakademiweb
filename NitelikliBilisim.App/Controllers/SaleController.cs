35d16f584189b8d4123c1168aa0b8bd6ad369c99
aksungursanberk <sanberkaksungur@outlook.com>
2020-01-16 11:33:48 +0300

    Satış İşlemleri #66

---
+++ NitelikliBilisim.App/Controllers/SaleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NitelikliBilisim.App.Controllers
+{
+    public class SaleController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Payment()
+        {
+            return View();
+        }
+    }
+}
