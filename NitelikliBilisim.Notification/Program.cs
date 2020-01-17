b762d03be1bf03331462efe98da030a645690719
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2020-01-17 11:40:24 +0300

    Notify

---
+++ NitelikliBilisim.Notification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NitelikliBilisim.Notification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
