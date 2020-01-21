008f015d0257ae075622e04255eaac0c5217f1df
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2020-01-21 15:41:50 +0300

    :snake: OK!

--- NitelikliBilisim.Core/ViewModels/areas/admin/educator/UpdateVm.cs
+++ NitelikliBilisim.Core/ViewModels/areas/admin/educator/UpdateVm.cs
@@ -1,8 +1,5 @@
-﻿using NitelikliBilisim.Core.Entities;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace NitelikliBilisim.Core.ViewModels.areas.admin.educator
 {
