c0a1fa9b86437f0932023f1e43bcf6e82f4f53b3
esercanikk <canikeser@gmail.com>
2020-01-28 11:04:33 +0300

    #88 egitim-kazanim-yonetimi

---
+++ NitelikliBilisim.Core/ViewModels/areas/admin/education_gains/UpdateVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NitelikliBilisim.Core.ViewModels.areas.admin.education_gains
+{
+    public class UpdateGetVm : AddGainVm
+    {
+        public Guid Id { get; set; }
+        public string EducationName { get; set; }
+    }
+    public class UpdatePostVm : AddGainVm
+    {
+        public Guid GainId { get; set; }
+    }
+}
