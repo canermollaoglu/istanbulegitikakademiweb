78e866266e14868ec2ee2ee9f3bc6ed00814427d
esercanikk <canikeser@gmail.com>
2020-01-29 11:20:21 +0300

    #88 admin/egitim-guncelle

--- NitelikliBilisim.App/Areas/Admin/Controllers/EducationController.cs
+++ NitelikliBilisim.App/Areas/Admin/Controllers/EducationController.cs
@@ -125,47 +125,6 @@ namespace NitelikliBilisim.App.Areas.Admin.Controllers
                     errors = ModelStateUtil.GetErrors(ModelState)
                 });
 
-            if(data.BannerFile.Base64Content != null){
-                var oldBanner = _unitOfWork.EducationMedia.Get().FirstOrDefault(x => x.EducationId == data.EducationId && x.MediaType == EducationMediaType.Banner);
-                if (oldBanner != null)
-                {
-                    var oldBannerPath = oldBanner.FileUrl;
-                    _fileManager.Delete(oldBannerPath);
-
-                    _unitOfWork.EducationMedia.Delete(oldBanner);
-                }
-
-                var bannerStream = new MemoryStream(_fileManager.ConvertBase64StringToByteArray(data.BannerFile.Base64Content));
-                var bannerFileName = $"{data.Name.FormatForTag()}-banner";
-                var bannerPath = await _storage.UploadFile(bannerStream, $"{bannerFileName}.{data.BannerFile.Extension.ToLower()}", "media-items");
-                var banner = new EducationMedia
-                {
-                    EducationId = data.EducationId,
-                    FileUrl = bannerPath,
-                    MediaType = EducationMediaType.Banner
-                };
-                _unitOfWork.EducationMedia.Insert(banner);
-            }
-
-            if (data.PreviewFile.Base64Content != null){
-                var oldPreview = _unitOfWork.EducationMedia.Get().First(x => x.EducationId == data.EducationId && x.MediaType == EducationMediaType.PreviewPhoto);
-
-                var oldPreviewPath = oldPreview.FileUrl;
-                _fileManager.Delete(oldPreviewPath);
-
-                _unitOfWork.EducationMedia.Delete(oldPreview);
-
-                var previewStream = new MemoryStream(_fileManager.ConvertBase64StringToByteArray(data.PreviewFile.Base64Content));
-                var previewFileName = $"{data.Name.FormatForTag()}-preview";
-                var previewPath = await _storage.UploadFile(previewStream, $"{previewFileName}.{data.PreviewFile.Extension.ToLower()}", "media-items");
-                var preview = new EducationMedia
-                {
-                    FileUrl = previewPath,
-                    MediaType = data.PreviewFile.Extension == "mp4" ? EducationMediaType.PreviewVideo : EducationMediaType.PreviewPhoto
-                };
-                _unitOfWork.EducationMedia.Insert(preview);
-            }
-
             _vmCreator.SendVmToUpdate(data );
 
             return Json(new ResponseModel
