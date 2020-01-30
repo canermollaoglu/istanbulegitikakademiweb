b53f8731459e1aab501c6eb4e226085ad472f05c
aksungursanberk <sanberkaksungur@outlook.com>
2020-01-30 15:29:41 +0300

    [Prod v1.0 Ready] Satışlar #109

--- NitelikliBilisim.Core/Entities/sales/Ticket.cs
+++ NitelikliBilisim.Core/Entities/sales/Ticket.cs
@@ -14,12 +14,20 @@ namespace NitelikliBilisim.Core.Entities
         }
 
         public bool IsUsed { get; set; }
+
+        [ForeignKey("Education")]
+        public Guid EducationId { get; set; }
+        public virtual Education Education { get; set; }
+
         [MaxLength(450), ForeignKey("Owner")]
         public string OwnerId { get; set; }
         public virtual Customer Owner { get; set; }
+
         [ForeignKey("Host")]
         public Guid HostId { get; set; }
         public virtual EducationHost Host { get; set; }
+
         public Guid InvoiceDetailsId { get; set; }
+        public Guid? GroupId { get; set; }
     }
 }
