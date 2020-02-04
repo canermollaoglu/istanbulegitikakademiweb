f5e2a132755ae8e81c46a145d3369c131b8551a2
Alper Dağlı <alper379@gmail.com>
2020-02-04 12:36:45 +0300

    /egitimler

--- NitelikliBilisim.App/wwwroot/js/pages/browser-courses.js
+++ NitelikliBilisim.App/wwwroot/js/pages/browser-courses.js
@@ -82,7 +82,7 @@ function listingFilters_onClick() {
     var order = $(this).val();
 
     if (order == 0)
-        location.href = 'egitimler';
+        location.href = '/egitimler';
 
     if (_hasFilter)
         getFilteredResults(false, order);
