534b56facba02fa4e12f23a4592d2f68ee1869a5
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2020-02-04 16:16:36 +0300

    #115 gerekli enumlar eklendi

--- NitelikliBilisim.Core/ViewModels/Sales/PayVm.cs
+++ NitelikliBilisim.Core/ViewModels/Sales/PayVm.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
 using Iyzipay.Model;
+using NitelikliBilisim.Core.Services.Payments;
 
 namespace NitelikliBilisim.Core.ViewModels.Sales
 {
@@ -75,7 +75,14 @@ namespace NitelikliBilisim.Core.ViewModels.Sales
         public string Ip { get; set; }
         public string IdentityNumber { get; set; } = "12345678901";
     }
-
+    public enum CardTypes
+    {
+        CREDIT_CARD = 1, DEBIT_CARD = 100, PREPAID_CARD = 500
+    }
+    public enum CardAssociations
+    {
+        TROY = 10, VISA = 200, MASTER_CARD = 100, AMERICAN_EXPRESS = 1000
+    }
     public enum CreditCardTypes
     {
         TROY = 10,
@@ -83,7 +90,10 @@ namespace NitelikliBilisim.Core.ViewModels.Sales
         VISA = 200,
         AMEX = 1000
     }
-
+    public enum CardFamilyNames
+    {
+        Bonus = 10, Axess = 40, World = 20, Maximum = 30, Paraf = 60, CardFinans = 50, Advantage = 70
+    }
     public enum CreditCardPrograms
     {
         BONUS = 10,
@@ -94,5 +104,4 @@ namespace NitelikliBilisim.Core.ViewModels.Sales
         PARAF = 60,
         ADVANTAGE = 70
     }
-
 }
