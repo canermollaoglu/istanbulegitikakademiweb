eaf38e41db1f130c95476c9a1f812fed08d429cc
aksungursanberk <sanberkaksungur@outlook.com>
2020-02-14 12:03:44 +0300

    Kullanıcı Profili #122

---
+++ NitelikliBilisim.App/Controllers/UserProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NitelikliBilisim.App.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        [Route("profil/{userId}")]
+        public IActionResult Profile(string userId)
+        {
+            if (userId == null)
+                return Redirect("/");
+
+            return View();
+        }
+    }
+}
