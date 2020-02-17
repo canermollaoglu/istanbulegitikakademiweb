d082d4f1ff00faf6c2b874b1326a026e2baee6ed
aksungursanberk <sanberkaksungur@outlook.com>
2020-02-17 11:46:57 +0300

    [Prod v1.0 Ready] Gruplar #108

---
+++ NitelikliBilisim.Core/Entities/educations/GroupAttendance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NitelikliBilisim.Core.Entities
+{
+    [Table("GroupAttendances")]
+    public class GroupAttendance
+    {
+        public Guid GroupId { get; set; }
+        public DateTime Date { get; set; }
+        [MaxLength(450)]
+        public string CustomerId { get; set; }
+    }
+}
