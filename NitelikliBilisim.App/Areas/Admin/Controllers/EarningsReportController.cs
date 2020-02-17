daa9547760714fe64b267a9607c8e3ab81decebe
aksungursanberk <sanberkaksungur@outlook.com>
2020-02-17 14:01:18 +0300

    Satış Raporları #123

---
+++ NitelikliBilisim.App/Areas/Admin/Controllers/EarningsReportController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NitelikliBilisim.App.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize(Roles = "Admin")]
+    public class EarningsReportController : Controller
+    {
+        [Route("raporlar/satis")]
+        public IActionResult IncomeReport()
+        {
+            return View();
+        }
+    }
+}
