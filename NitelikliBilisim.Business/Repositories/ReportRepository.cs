07c728c0897ad7076044501a3fcc38773dd9fb4d
aksungursanberk <sanberkaksungur@outlook.com>
2020-02-18 11:09:25 +0300

    Satış Raporları #123

--- NitelikliBilisim.Business/Repositories/ReportRepository.cs
+++ NitelikliBilisim.Business/Repositories/ReportRepository.cs
@@ -24,26 +24,30 @@ namespace NitelikliBilisim.Business.Repositories
                 .ThenInclude(x => x.Invoice)
                 .ThenInclude(x => x.Customer)
                 .ThenInclude(x => x.User)
-                //.Where(x => x.BlockageResolveDate.Year == year && x.BlockageResolveDate.Month == month)
-                .Where(x =>
-                x.BlockageResolveDate.AddMonths(x.InvoiceDetail.Invoice.PaymentCount - 1).AddDays(7).Year == year
-                && x.BlockageResolveDate.AddMonths(x.InvoiceDetail.Invoice.PaymentCount - 1).AddDays(7).Month == month)
+                .Where(x => x.BlockageResolveDate.Year == year - 1 || x.BlockageResolveDate.Year == year)
                 .ToList();
 
             var payouts = new List<_Payout>();
             foreach (var item in data)
             {
                 var paymentCount = item.InvoiceDetail.Invoice.PaymentCount;
-                var payout = !item.IsCancelled ? item.MerchantPayout / paymentCount : item.MerchantPayout / paymentCount * -1;
-                payouts.Add(new _Payout
+                for (int i = 0; i < paymentCount; i++)
                 {
-                    PayoutNumeric =  payout,
-                    PayoutText = payout.ToString("C", CultureInfo.CreateSpecificCulture("tr-TR")),
-                    IsNegative = payout < 0,
-                    PayoutDate = item.BlockageResolveDate.AddDays(7),
-                    PayoutDateText = item.BlockageResolveDate.AddDays(7).ToShortDateString(),
-                    Payer = $"{item.InvoiceDetail.Invoice.Customer.User.Name.ToUpper()} {item.InvoiceDetail.Invoice.Customer.User.Surname.ToUpper()}"
-                });
+                    var blockageResolveDate = item.BlockageResolveDate.AddMonths(i);
+                    if (blockageResolveDate.Month == month)
+                    {
+                        var payout = !item.IsCancelled ? item.MerchantPayout / paymentCount : item.MerchantPayout / paymentCount * -1;
+                        payouts.Add(new _Payout
+                        {
+                            PayoutNumeric = payout,
+                            PayoutText = payout.ToString("C", CultureInfo.CreateSpecificCulture("tr-TR")),
+                            IsNegative = payout < 0,
+                            PayoutDate = item.BlockageResolveDate.AddMonths(i),
+                            PayoutDateText = item.BlockageResolveDate.AddMonths(i).ToShortDateString(),
+                            Payer = $"{item.InvoiceDetail.Invoice.Customer.User.Name.ToUpper()} {item.InvoiceDetail.Invoice.Customer.User.Surname.ToUpper()}"
+                        });
+                    }
+                }
             }
 
             var model = new IncomeReportVm
