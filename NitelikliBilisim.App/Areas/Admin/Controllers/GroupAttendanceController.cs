eac27a9f6ecad4c572f1629c45995bc10c9be66b
aksungursanberk <sanberkaksungur@outlook.com>
2020-02-27 11:09:23 +0300

    Grup İşlemleri (genel) #138

---
+++ NitelikliBilisim.App/Areas/Admin/Controllers/GroupAttendanceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NitelikliBilisim.App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class GroupAttendanceController : Controller
+    {
+        [Route("yoklama-girisi/{groupId?}")]
+        public IActionResult ManageGroupAttendance(Guid? groupId)
+        {
+            if (!groupId.HasValue)
+                return Redirect("/");
+
+            return View();
+        }
+    }
+}
