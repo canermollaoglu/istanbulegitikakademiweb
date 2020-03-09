85a4db18fd77ecdf4b6177dfb72748f3c2f3511c
Alper Dağlı <alper379@gmail.com>
2020-03-09 15:15:33 +0300

    mailtest

---
+++ NitelikliBilisim.Test/NotificatorTests/MailTests.cs
@@ -0,0 +1,30 @@
+﻿using NitelikliBilisim.Core.ComplexTypes;
+using NitelikliBilisim.Notificator.Services;
+using Xunit;
+
+namespace NitelikliBilisim.Test.NotificatorTests
+{
+    public class MailTests
+    {
+        private readonly EmailSender _emailSender;
+        private static readonly string[] mailsToSend =  { "alper.dagli@wissenakademie.com", "eser.canik@wissenakademie.com" };
+
+        public MailTests()
+        {
+            _emailSender = new EmailSender();
+        }
+
+        [Fact]
+        public void Should_Send_Email()
+        {
+            var result = _emailSender.SendAsync(new EmailMessage()
+            {
+                Subject = "Test Mail | Nitelikli Bilişim",
+                Body = "Test",
+                Contacts = mailsToSend
+            });
+
+            Assert.True(result.IsCompletedSuccessfully);
+        }
+    }
+}
