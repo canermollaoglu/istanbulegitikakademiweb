ade8f7093c84cd37171f259a3b2cfd7d9ef6df64
aksungursanberk <sanberkaksungur@outlook.com>
2020-03-10 17:17:43 +0300

    Eğitmen Ödemeleri #139

---
+++ NitelikliBilisim.Business/Repositories/EducatorSalaryRepository.cs
@@ -0,0 +1,28 @@
+﻿using NitelikliBilisim.Core.Entities;
+using NitelikliBilisim.Core.ViewModels.areas.admin.educator_salary;
+using NitelikliBilisim.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NitelikliBilisim.Business.Repositories
+{
+    public class EducatorSalaryRepository : BaseRepository<EducatorSalary, Guid>
+    {
+        private readonly NbDataContext _context;
+        public EducatorSalaryRepository(NbDataContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EnterSalaryVm GetSalaries(DateTime date)
+        {
+            var model = new EnterSalaryVm
+            {
+                Salaries = new List<_Salary>()
+            };
+
+            return model;
+        }
+    }
+}
