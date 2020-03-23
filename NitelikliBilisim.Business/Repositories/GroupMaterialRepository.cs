1111fcab23a1ef9f4e17124d906661a377cdb92f
aksungursanberk <sanberkaksungur@outlook.com>
2020-03-23 11:07:25 +0300

    Grup Materyalleri #142

--- NitelikliBilisim.Business/Repositories/GroupMaterialRepository.cs
+++ NitelikliBilisim.Business/Repositories/GroupMaterialRepository.cs
@@ -1,7 +1,10 @@
 ﻿using NitelikliBilisim.Core.Entities;
+using NitelikliBilisim.Core.ViewModels.areas.admin.group_material;
 using NitelikliBilisim.Data;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 
 namespace NitelikliBilisim.Business.Repositories
@@ -12,6 +15,43 @@ namespace NitelikliBilisim.Business.Repositories
         public GroupMaterialRepository(NbDataContext context) : base(context)
         {
             _context = context;
+        }
+
+        public GetMaterialVm GetMaterials(Guid groupId)
+        {
+            var model = new GetMaterialVm
+            {
+                Materials = new List<_Material>()
+            };
+            var materials = _context.GroupMaterials
+                .Where(x => x.GroupId == groupId)
+                .ToList();
+
+            model.Materials = materials.Select(x => new _Material
+                {
+                    MaterialId = x.Id,
+                    GroupId = x.GroupId,
+                    MaterialName = x.MaterialName,
+                    MaterialPrice = x.Price.ToString("C", CultureInfo.CreateSpecificCulture("tr-TR")),
+                    MaterialCount = $"{x.Count} adet"
+                }).ToList();
+            return model;
+        }
+        public bool AddMaterial(AddMaterialPostVm data)
+        {
+            if (string.IsNullOrWhiteSpace(data.MaterialName) || data.Count < 1 || data.Price < 0)
+                return false;
+
+            _context.GroupMaterials.Add(new GroupMaterial
+            {
+                Count = data.Count,
+                GroupId = data.GroupId,
+                MaterialName = data.MaterialName,
+                Price = data.Price
+            });
+
+            _context.SaveChanges();
+            return true;
         }
     }
 }
---
+++ NitelikliBilisim.Core/ViewModels/areas/admin/group_material/GetMaterialVm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NitelikliBilisim.Core.ViewModels.areas.admin.group_material
+{
+    public class GetMaterialVm
+    {
+        public List<_Material> Materials { get; set; }
+    }
+
+    public class _Material
+    {
+        public Guid MaterialId { get; set; }
+        public string MaterialName { get; set; }
+        public string MaterialPrice { get; set; }
+        public string MaterialCount { get; set; }
+        public Guid GroupId { get; set; }
+    }
+}
