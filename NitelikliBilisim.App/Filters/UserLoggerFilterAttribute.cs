73ab25c433a9c6a86adb6f210208fae2167c15e6
Mesut Öztürk <mesutoztrk@msn.com>
2020-07-16 18:53:25 +0300

    .

--- NitelikliBilisim.App/Filters/UserLoggerFilterAttribute.cs
+++ NitelikliBilisim.App/Filters/UserLoggerFilterAttribute.cs
@@ -4,17 +4,11 @@ using NitelikliBilisim.Core.ComplexTypes;
 using System;
 using System.Security.Claims;
 using Serilog;
-using Serilog.Events;
-using Serilog.Formatting.Elasticsearch;
-using Serilog.Sinks.Elasticsearch;
 
 namespace NitelikliBilisim.App.Filters
 {
     public class UserLoggerFilterAttribute : Attribute, IActionFilter
     {
-        
-        
-
         public void OnActionExecuted(ActionExecutedContext context)
         {
             Console.WriteLine(DateTime.Now);
@@ -29,15 +23,14 @@ namespace NitelikliBilisim.App.Filters
                 ActionName = descriptor.ActionName,
                 SessionId = context.HttpContext.Session.Id,
                 IpAddress = context.HttpContext.Connection.RemoteIpAddress.ToString()
-        };
+            };
             if (context.HttpContext.User.Identity.IsAuthenticated)
             {
                 var userId = context.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
                 log.UserId = userId;
             }
-            Log.Information("Processed {@log} in {Time} ms", log,DateTime.Now);
+            Log.Information("Processed {@log} in {Time} ms", log, DateTime.Now);
 
         }
-
     }
 }
