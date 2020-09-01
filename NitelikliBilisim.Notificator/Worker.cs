f4c8ccec9afc67b24132bd42ff7eede8957df63b
Mesut OZTURK <mesutoztrk@msn.com>
2020-09-01 12:22:58 +0300

    .

--- NitelikliBilisim.Notificator/Worker.cs
+++ NitelikliBilisim.Notificator/Worker.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NitelikliBilisim.Notificator.Services;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace NitelikliBilisim.Notificator
 {
