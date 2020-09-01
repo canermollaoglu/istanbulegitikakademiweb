c28973506c0bc9173aee5280652284a24d52151f
Asım ULUSOY <ulusoy.asim@gmail.com>
2020-09-01 09:53:12 +0300

    #189 Grup Gider Tipleri
    Grup Gider Tipleri için CRUD işlemleri tamamlandı.

---
+++ NitelikliBilisim.App/Areas/Admin/Controllers/WebAPI/GroupExpenseTypeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevExtreme.AspNet.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NitelikliBilisim.App.Extensions;
+using NitelikliBilisim.Business.UoW;
+
+namespace NitelikliBilisim.App.Areas.Admin.Controllers.WebAPI
+{
+    
+    public class GroupExpenseTypeController : BaseApiController
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public GroupExpenseTypeController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("get-group-expense-types")]
+        public IActionResult GetGroupExpenseTypes(DataSourceLoadOptions loadOptions)
+        {
+            loadOptions.PrimaryKey = new[] { "Id" };
+            var data = _unitOfWork.GroupExpenseType.Get();
+            return Ok(DataSourceLoader.Load(data, loadOptions));
+        }
+    }
+}
---
+++ NitelikliBilisim.App/wwwroot/js/areas/admin/admin-group-expense-type-update.js
@@ -0,0 +1,54 @@
+﻿/* elements */
+var inputName = document.getElementById("input-name");
+var inputDescription = document.getElementById("input-description");
+var btnSave = $("#btn-save");
+
+/* assignments */
+$(document).ready(document_onLoad);
+btnSave.on("click", btnSave_onClick);
+
+/* events */
+function document_onLoad() {
+}
+function btnSave_onClick() {
+    btnSave.off("click");
+    var data = {
+        Id: $("#expense-type-id").val(),
+        Name: inputName.value,
+        Description: inputDescription.value
+    }
+
+    var tokenVerfier = new SecuritySupport.TokenVerifier();
+    data = tokenVerfier.addToken("form-update-group-expense-type", data);
+
+    $.ajax({
+        url: "/admin/groupexpensetype/update",
+        method: "post",
+        data: data,
+        success: (res) => {
+            var resultAlert = new AlertSupport.ResultAlert();
+            if (res.isSuccess) {
+                resultAlert.display({
+                    success: true,
+                    message: "İşlem başarılı. Gider tipleri listesine dönmek için {link}",
+                    redirectElement: {
+                        content: "tıklayınız",
+                        link: "/admin/groupexpensetype/list"
+                    }
+                });
+            } else {
+                resultAlert.display({
+                    success: false,
+                    errors: res.errors,
+                    message: "Sayfayı yenilemek için {link}",
+                    redirectElement: {
+                        content: "tıklayınız",
+                        link: location.href
+                    }
+                });
+            }
+
+            btnSave.on("click", btnSave_onClick);
+        }
+    });
+}
