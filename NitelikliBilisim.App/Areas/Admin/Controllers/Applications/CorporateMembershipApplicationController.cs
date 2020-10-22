73b414a14bd344aa0e2752c24cf25d71518933ac
Asım ULUSOY <ulusoy.asim@gmail.com>
2020-10-22 15:08:07 +0300

    #226
    Görüldü bilgisi eklendi.

--- NitelikliBilisim.App/Areas/Admin/Controllers/Applications/CorporateMembershipApplicationController.cs
+++ NitelikliBilisim.App/Areas/Admin/Controllers/Applications/CorporateMembershipApplicationController.cs
@@ -5,16 +5,52 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NitelikliBilisim.App.Lexicographer;
+using NitelikliBilisim.App.Models;
+using NitelikliBilisim.Business.UoW;
 
 namespace NitelikliBilisim.App.Areas.Admin.Controllers.Applications
 {
     [Area("Admin"), Authorize(Roles = "Admin")]
     public class CorporateMembershipApplicationController : Controller
     {
+        private readonly UnitOfWork _unitOfWork;
+        public CorporateMembershipApplicationController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult List()
         {
             ViewData["bread_crumbs"] = BreadCrumbDictionary.ReadPart("AdminCorporateMembershipApplicationList");
             return View();
         }
+
+        /// <summary>
+        /// Kayıt görüldü bilgisini true olarak günceller.
+        /// </summary>
+        /// <param name="cMAID">corporateMembershipId</param>
+        /// <returns></returns>
+        public IActionResult SetViewedStatus(Guid cMAID)
+        {
+            try
+            {
+                var corporateMembershipApplication = _unitOfWork.CorporateMembershipApplication.GetById(cMAID);
+                corporateMembershipApplication.IsViewed = true;
+                _unitOfWork.CorporateMembershipApplication.Update(corporateMembershipApplication);
+                return Json(new ResponseModel
+                {
+                    isSuccess = true
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseModel
+                {
+                    isSuccess = false,
+                    errors = new List<string> { $"Hata: {ex.Message}" }
+                });
+            }
+        }
+
     }
 }
