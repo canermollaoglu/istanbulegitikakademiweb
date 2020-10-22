e7c8ee2c768c2d7761bad908df7dce2bc550f487
Asım ULUSOY <ulusoy.asim@gmail.com>
2020-10-22 10:08:18 +0300

    Eğitmen ve Kurumsal Başvurular
    Eğitmen başvurusu ve Kurumsal başvuru için ilgili modeller oluşturuldu.

---
+++ NitelikliBilisim.Core/Entities/CorporateMembershipApplication.cs
@@ -0,0 +1,28 @@
+﻿using NitelikliBilisim.Core.Abstracts;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NitelikliBilisim.Core.Entities
+{
+    public class CorporateMembershipApplication : BaseEntity<Guid>
+    {
+        public CorporateMembershipApplication()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Required, MaxLength(150)]
+        public string CompanyName { get; set; }
+        [Required, MaxLength(100)]
+        public string CompanySector { get; set; }
+        [Required, MaxLength(100)]
+        public string NameSurname { get; set; }
+        [Required, MaxLength(15)]
+        public string Phone { get; set; }
+        [Required, MaxLength(100)]
+        public string Department { get; set; }
+        public int NumberOfEmployees { get; set; }
+        public string Address { get; set; }
+        public string RequestNote { get; set; }
+    }
+}
---
+++ NitelikliBilisim.Core/Entities/EducatorApplication.cs
@@ -0,0 +1,25 @@
+﻿using NitelikliBilisim.Core.Abstracts;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NitelikliBilisim.Core.Entities
+{
+    public class EducatorApplication:BaseEntity<Guid>
+    {
+        public EducatorApplication()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Required, MaxLength(100)]
+        public string NameSurname { get; set; }
+        [Required, MaxLength(15)]
+        public string Phone { get; set; }
+        [Required, MaxLength(100)]
+        public string Email { get; set; }
+        [MaxLength(500)]
+        public string Note { get; set; }
+        public string CvUrl { get; set; }
+
+    }
+}
