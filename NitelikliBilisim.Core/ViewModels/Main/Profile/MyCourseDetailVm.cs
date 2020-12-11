3fe88b62bbbbc4522b8d24346ffa676054217bff
Asım ULUSOY <ulusoy.asim@gmail.com>
2020-12-11 12:58:46 +0300

    #236
    -Kullanıcı yorumları için statu column eklendi.
    -Hesap sayfasında yer alan sidebar componente ilgili sayfa ismi gönderilerek active classının eklenmesi sağlandı.
    -Yorumlarım sayfası oluşturuldu.
    -Kurslarım sayfası oluşturuldu.
    -Kurs detaylarım sayfası oluşturuldu.
    -ilgili backend kodları yazıldı.

---
+++ NitelikliBilisim.Core/ViewModels/Main/Profile/MyCourseDetailVm.cs
@@ -0,0 +1,26 @@
+﻿using NitelikliBilisim.Core.Entities.user_details;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitelikliBilisim.Core.ViewModels.Main.Profile
+{
+    public class MyCourseDetailVm
+    {
+        public Guid EducationId { get; set; }
+        public string EducatorId { get; set; }
+        public string EducationName { get; set; }
+        public decimal? PriceText { get; set; }
+        public string Days { get; set; }
+        public string Hours { get; set; }
+        public string Host { get; set; }
+        public string EducatorName { get; set; }
+        public string EducatorTitle { get; set; }
+        public string EducatorAvatarPath { get; set; }
+        public string EducationFeaturedImage { get; set; }
+        public string EducationShortDescription { get; set; }
+        public List<EducatorCertificate> EducatorCertificates { get; set; }
+    }
+}
---
+++ NitelikliBilisim.Core/ViewModels/Main/Profile/MyCourseVm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitelikliBilisim.Core.ViewModels.Main.Profile
+{
+    public class MyCourseVm
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Days { get; set; }
+        public string Hours { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public Guid EducationId { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
