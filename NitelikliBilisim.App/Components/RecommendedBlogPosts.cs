0492a4b8df9c5cffbe9b60895bd460889c174ac9
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2020-12-22 11:50:37 +0300

    .

--- NitelikliBilisim.App/Components/RecommendedBlogPosts.cs
+++ NitelikliBilisim.App/Components/RecommendedBlogPosts.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NitelikliBilisim.Business.UoW;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NitelikliBilisim.App.Components
 {
