4069e5fa8e2161c66c45f894c68dd054e39dec42
Asım ULUSOY <ulusoy.asim@gmail.com>
2020-12-30 14:48:22 +0300

    #236
    Admin tarafında satın alım yapmış öğrenciler için fatura listesi geliştirildi. Fatura listesinden fiziki faturanın pdf olarak yüklenmesi için gerekli operasyonlar tamamlandı.

---
+++ NitelikliBilisim.Core/ViewModels/areas/admin/invoice/InvoiceListVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitelikliBilisim.Core.ViewModels.areas.admin.invoice
+{
+    public class InvoiceListVm
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public decimal PaidPrice { get; set; }
+        public string PaymentId { get; set; }
+        public string InvoicePdfUrl { get; set; }
+    }
+}
