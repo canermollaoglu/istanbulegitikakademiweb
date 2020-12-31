6c10dbff32be494d435c58fbe1ab3f5fa3b38b41
Asım ULUSOY <ulusoy.asim@gmail.com>
2020-12-31 09:55:24 +0300

    #236
    -Grup eski fiyatının admin tarafından yönetilebilmesi için gerekli geliştirmeler yapıldı.
    -Fiyat hesaplama sihirbazından çıkan fiyatın kaydolması için yalnızca yeni fiyatı kaydedecek bir action yazıldı.

--- NitelikliBilisim.Core/ViewModels/areas/admin/education_groups/UpdateGroupGeneralInformationVm.cs
+++ NitelikliBilisim.Core/ViewModels/areas/admin/education_groups/UpdateGroupGeneralInformationVm.cs
@@ -9,11 +9,10 @@ namespace NitelikliBilisim.Core.ViewModels.areas.admin.education_groups
     {
         [Required]
         public Guid GroupId { get; set; }
-        [Required]
         [MinLength(3,ErrorMessage ="Grup adı minimum 3 karakterden oluşmalıdır.")]
         public string GroupName { get; set; }
-        [Required]
         public decimal NewPrice { get; set; }
+        public decimal OldPrice { get; set; }
 
     }
 }
---
+++ NitelikliBilisim.Core/ViewModels/areas/admin/education_groups/UpdateGroupNewPriceVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitelikliBilisim.Core.ViewModels.areas.admin.education_groups
+{
+    public class UpdateGroupNewPriceVm
+    {
+        public Guid GroupId { get; set; }
+        public decimal? NewPrice { get; set; }
+    }
+}
