5b5fcf1f7813aaa552ec140468f4049358b642e1
Asım ULUSOY <ulusoy.asim@gmail.com>
2021-01-13 12:51:32 +0300

    #236
    -Admin login olduğunda admin paneline yönlendirildi.
    -Anasayfada bulunan popüler teknoloji kategorileri alanı veritabanından beslenecek şekilde yapılandırıldı.
    -Menu elemanları düzenlendi.

--- NitelikliBilisim.App/Models/HomeIndexModel.cs
+++ NitelikliBilisim.App/Models/HomeIndexModel.cs
@@ -8,7 +8,7 @@ namespace NitelikliBilisim.App.Models
     public class HomeIndexModel
     {
         public List<SuggestedEducationVm> SuggestedEducations { get; set; }
-        public Dictionary<string, int> EducationCountByCategory { get; set; }
+        public List<HomePageCategoryVm> EducationCountByCategory { get; set; }
         public List<HighlightCommentVm> EducationComments { get; internal set; }
         public Dictionary<int, string> HostCities { get; internal set; }
         public List<EducationSearchTag> EducationSearchTags { get; internal set; }
---
+++ NitelikliBilisim.Core/ViewModels/Main/Home/HomePageCategoryVm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitelikliBilisim.Core.ViewModels.Main.Home
+{
+    public class HomePageCategoryVm
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string IconUrl { get; set; }
+        public int EducationCount { get; set; }
+        public string IconColor { get; set; }
+        public string SeoUrl { get; set; }
+    }
+}
