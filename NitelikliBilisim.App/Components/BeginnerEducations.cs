8358cb21dd331b004330f37d6f99c92f309236c8
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2021-01-19 13:14:22 +0300

    :rocket:

--- NitelikliBilisim.App/Components/BeginnerEducations.cs
+++ NitelikliBilisim.App/Components/BeginnerEducations.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NitelikliBilisim.Business.UoW;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NitelikliBilisim.App.Components
 {
